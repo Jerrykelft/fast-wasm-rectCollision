61608afb2ab0072f99ee4d6c3dcdf1fff0f39e97
Jerrykelft <yuli873061@gmail.com>
2025-10-25 23:08:08 +0800

    update

--- rectCollision-f32-simd.c
+++ rectCollision-f32-simd.c
@@ -3,12 +3,10 @@
 
 #define true 1
 #define false 0
-#define rect_param(__NAME__) Vector2 __NAME__[static restrict 4] // 矩形
-#define f32x4_get(a, i) wasm_f32x4_extract_lane(a, i) // 取出 v128 的第 i 個 float
+#define RECT_PARAM(__NAME__) Vector2 __NAME__[static restrict 4] // 矩形
 
 typedef struct {float x, y;} Vector2; // 向量
 typedef Vector2 RectVertex[4]; // 矩形頂點
-typedef struct {float min, max;} Project; // 投影
 
 #ifdef __INTELLISENSE__
 // 這段只給 VSCode IntelliSense 看
@@ -16,7 +14,7 @@ v128_t __builtin_wasm_shuffle_i8x16(v128_t, v128_t, int, int, int, int, int, int
 #endif
 
 // 獲取無旋轉矩形
-static inline void _getRightRect(rect_param(result), float x, float y, float w, float h) {
+static inline void _getRightRect(RECT_PARAM(result), float x, float y, float w, float h) {
     float hw = w / 2.0f, hh = h / 2.0f;
     result[0].x = result[3].x = x - hw;
     result[1].y = result[0].y = y - hh;
@@ -25,7 +23,7 @@ static inline void _getRightRect(rect_param(result), float x, float y, float w, 
 }
 
 // 獲取頂點
-static inline void _getRect(rect_param(result), float x, float y, float w, float h, float r) {
+static inline void _getRect(RECT_PARAM(result), float x, float y, float w, float h, float r) {
     float hw = w / 2.0f, hh = h / 2.0f;
     float c = cos(r), s = sin(r);
     float hw_c = hw * c, hh_s = hh * s, hw_s = hw * s, hh_c = hh * c;
@@ -45,20 +43,8 @@ static inline void _pointRotateAround(float px[static restrict 1], float py[stat
     *py = x * s + y * c + cy;
 }
 
-// 將多邊形投影到軸上
-static inline Project _projectPolygon(Vector2 axis, rect_param(rect)) {
-    float min = axis.x * rect[0].x + axis.y * rect[0].y;
-    float max = min;
-    for (int i = 1; i < 4; i++) {
-        float p = axis.x * rect[i].x + axis.y * rect[i].y;
-        if (p < min) min = p;
-        if (p > max) max = p;
-    }
-    return (Project){min, max};
-}
-
 // 計算矩形邊界
-static inline void _rectBounds(rect_param(rect), Vector2 min[static restrict 1], Vector2 max[static restrict 1]) {
+static inline void _rectBounds(RECT_PARAM(rect), Vector2 min[static restrict 1], Vector2 max[static restrict 1]) {
     min->x = fmin(fmin(rect[0].x, rect[1].x), fmin(rect[2].x, rect[3].x));
     min->y = fmin(fmin(rect[0].y, rect[1].y), fmin(rect[2].y, rect[3].y));
     max->x = fmax(fmax(rect[0].x, rect[1].x), fmax(rect[2].x, rect[3].x));
@@ -66,7 +52,7 @@ static inline void _rectBounds(rect_param(rect), Vector2 min[static restrict 1],
 }
 
 // 包圍盒檢測
-static inline bool _boundingCollision(rect_param(rect1), rect_param(rect2)) {
+static inline bool _boundingCollision(RECT_PARAM(rect1), RECT_PARAM(rect2)) {
     Vector2 min1, max1, min2, max2;
     _rectBounds(rect1, &min1, &max1);
     _rectBounds(rect2, &min2, &max2);
@@ -74,8 +60,8 @@ static inline bool _boundingCollision(rect_param(rect1), rect_param(rect2)) {
 }
 
 // AABB vs OBB SAT 判定，AABB 為 rect1(未旋轉)，OBB 為 rect2(已旋轉)
-static inline bool _satAABBvsOBB(rect_param(rect1), rect_param(rect2)) {
-    if (!_boundingCollision(rect1, rect2)) return 0;
+static inline bool _satAABBvsOBB(RECT_PARAM(rect1), RECT_PARAM(rect2)) {
+    if (!_boundingCollision(rect1, rect2)) return false;
 
     // 使用 OBB 的邊產生分離軸
     Vector2 axes[2];
@@ -101,12 +87,19 @@ static inline bool _satAABBvsOBB(rect_param(rect1), rect_param(rect2)) {
         wasm_f32x4_add(wasm_f32x4_mul(vaxis1_x, vrect1_x), wasm_f32x4_mul(vaxis1_y, vrect1_y)),
         wasm_f32x4_add(wasm_f32x4_mul(vaxis1_x, vrect2_x), wasm_f32x4_mul(vaxis1_y, vrect2_y))
     };
-    v128_t a = wasm_f32x4_make(f32x4_get(projs[0], 0), f32x4_get(projs[1], 0), f32x4_get(projs[2], 0), f32x4_get(projs[3], 0));
-    v128_t b = wasm_f32x4_make(f32x4_get(projs[0], 1), f32x4_get(projs[1], 1), f32x4_get(projs[2], 1), f32x4_get(projs[3], 1));
-    v128_t c = wasm_f32x4_make(f32x4_get(projs[0], 2), f32x4_get(projs[1], 2), f32x4_get(projs[2], 2), f32x4_get(projs[3], 2));
-    v128_t d = wasm_f32x4_make(f32x4_get(projs[0], 3), f32x4_get(projs[1], 3), f32x4_get(projs[2], 3), f32x4_get(projs[3], 3));
-    v128_t vmin = wasm_f32x4_min(wasm_f32x4_min(a, b), wasm_f32x4_min(c, d));
-    v128_t vmax = wasm_f32x4_max(wasm_f32x4_max(a, b), wasm_f32x4_max(c, d));
+    // 交換相鄰行內的低兩個元素
+    v128_t t0 = wasm_v32x4_shuffle(projs[0], projs[1], 0, 4, 1, 5);
+    v128_t t1 = wasm_v32x4_shuffle(projs[0], projs[1], 2, 6, 3, 7);
+    v128_t t2 = wasm_v32x4_shuffle(projs[2], projs[3], 0, 4, 1, 5);
+    v128_t t3 = wasm_v32x4_shuffle(projs[2], projs[3], 2, 6, 3, 7);
+
+    // 交叉合併上下兩組
+    v128_t va = wasm_v32x4_shuffle(t0, t2, 0, 1, 4, 5);
+    v128_t vb = wasm_v32x4_shuffle(t0, t2, 2, 3, 6, 7);
+    v128_t vc = wasm_v32x4_shuffle(t1, t3, 0, 1, 4, 5);
+    v128_t vd = wasm_v32x4_shuffle(t1, t3, 2, 3, 6, 7);
+    v128_t vmin = wasm_f32x4_min(wasm_f32x4_min(va, vb), wasm_f32x4_min(vc, vd));
+    v128_t vmax = wasm_f32x4_max(wasm_f32x4_max(va, vb), wasm_f32x4_max(vc, vd));
 
     v128_t cmp = wasm_f32x4_lt(vmax, wasm_v32x4_shuffle(vmin, vmin, 1, 0, 3, 2));
     return !wasm_v128_any_true(cmp);
@@ -131,3 +124,13 @@ bool rectCollision(
     _getRect(rect2, x2, y2, w2, h2, newr2);
     return _satAABBvsOBB(rect1, rect2);
 }
+
+/*
+emsdk_env
+emcc -o rectCollision-f32-simd-tmp.wasm rectCollision-f32-simd.c --no-entry -O3 -msimd128 -flto -ffast-math -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_rectCollision']" -s INITIAL_MEMORY=131072
+wasm2wat rectCollision-f32-simd-tmp.wasm -o rectCollision-f32-simd-tmp.wat
+
+wat2wasm rectCollision-f32-simd-tmp.wat -o rectCollision-f32-simd-opt-tmp.wasm
+wasm-opt rectCollision-f32-simd-opt-tmp.wasm -O3 --enable-nontrapping-float-to-int --enable-simd --dce -o rectCollision-f32-simd.wasm
+wasm2wat rectCollision-f32-simd.wasm -o rectCollision-f32-simd.wat
+*/
