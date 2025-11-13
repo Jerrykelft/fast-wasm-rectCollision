4451ed96a5cd1c98849cd2d3e729cdd48f61b72e
Jerrykelft <yuli873061@gmail.com>
2025-11-13 21:55:58 +0800

    update

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,5 @@
 emsdk_env
-emcc -c simd.c -o simd.o -O3 -msimd128 -msse3 -flto -ffast-math -std=c23
+emcc -c simd.c -o simd.o -O3 -msimd128 -msse3 -flto -std=c23
 em++ -c rectCollision2D.cpp -o rectCollision.o -O3 -msimd128 -flto -std=c++23
 
 emcc simd.o rectCollision.o -o rectCollision2D.wasm -O3 -msimd128 -flto --no-entry -s INITIAL_MEMORY=131072 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_rectCollisionf', '_rectCollision']"
