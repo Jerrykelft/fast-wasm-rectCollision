a2106dca722af9a93077a1138aa0816e85a3f5e7
Jerrykelft <yuli873061@gmail.com>
2025-11-13 21:33:41 +0800

    update

--- compile3D.ps1
+++ compile3D.ps1
@@ -1,8 +1,8 @@
 emsdk_env
-emcc -c wasm_simd128++.c -o wasm_simd128++.o -O3 -msimd128 -msse3 -flto -ffast-math
-em++ -c rectCollision3D.cpp -o rectCollision3D.o -O3 -msimd128 -flto -ffast-math
-em++ wasm_simd128++.o rectCollision3D.o -o rectCollision3D.wasm -O3 -msimd128 -flto --no-entry -s INITIAL_MEMORY=131072 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_test1', '_test2']"
+emcc -c simd.c -o simd.o -O3 -msimd128 -msse3 -flto -ffast-math -std=c23
+em++ -c rectCollision3D.cpp -o rectCollision3D.o -O3 -msimd128 -flto -ffast-math -std=c++23
+em++ simd.o rectCollision3D.o -o rectCollision3D.wasm -O3 -msimd128 -flto --no-entry -s INITIAL_MEMORY=131072 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS="['_get', '_make', '_t1', '_t2']"
 wasm2wat rectCollision3D.wasm -o rectCollision3D.wat
-
-Remove-Item -Path "./wasm_simd128++.o"
+# -mrelaxed-simd -s ERROR_ON_UNDEFINED_SYMBOLS=1
+Remove-Item -Path "./simd.o"
 Remove-Item -Path "./rectCollision3D.o"
